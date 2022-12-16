f4981d64fbadc45cfcfab26ca635634efb167321
topgirl2410 <maria.topgirl82@gmail.com>
2022-12-16 23:32:49 +0100

    creando el routing y controlador par auth

---
+++ controllers/LoginController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Controllers;
+
+class LoginController
+{
+    public static function login()
+    {
+        echo "desde login";
+    }
+
+    public static function logout()
+    {
+        echo "desde logout";
+    }
+
+    public static function olvide()
+    {
+        echo "desde olvide";
+    }
+
+    public static function recuperar()
+    {
+        echo "desde recuperar";
+    }
+
+    public static function crear()
+    {
+        echo "desde crear";
+    }
+
+}
